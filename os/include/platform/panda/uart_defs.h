2fa1bed3100c12a9f97e33603399efd2389bd70a
Ivan Loskutov <loskutov.ivan@gmail.com>
2012-09-28 18:52:26 +0400

    Add OMAP4 UART driver

---
+++ os/include/platform/panda/uart_defs.h
@@ -0,0 +1,58 @@
+/*
+ * \brief  OMAP4 UART definitions
+ * \author Ivan Loskutov <ivan.loskutov@ksyslabs.org>
+ * \date   2012-09-28
+ */
+
+/*
+ * Copyright (C) 2012 Ksys Labs LLC
+ * Copyright (C) 2011-2012 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+#ifndef _INCLUDE__PLATFORM__PANDABOARD__UART_DEFS_H_
+#define _INCLUDE__PLATFORM__PANDABOARD__UART_DEFS_H_
+
+enum {
+
+	/** Number of UARTs */
+	UARTS_NUM = 4,
+
+	/**
+	 * MMIO regions
+	 */
+	UART0_MMIO_BASE = 0x4806a000,  /* base for UART 0 */
+	UART1_MMIO_BASE = 0x4806c000,  /* base for UART 1 */
+	UART2_MMIO_BASE = 0x48020000,  /* base for UART 2 */
+	UART3_MMIO_BASE = 0x4806e000,  /* base for UART 3 */
+	UARTS_MMIO_SIZE = 0x400,       /* size of each MMIO region */
+
+	/**
+	 * Interrupt lines
+	 */
+	UART0_IRQ = 72 + 32,      /* UART 0 */
+	UART1_IRQ = 73 + 32,      /* UART 1 */
+	UART2_IRQ = 74 + 32,      /* UART 2 */
+	UART3_IRQ = 70 + 32,      /* UART 3 */
+
+	/**
+	 * UART baud rate configuration (precalculated)
+	*/
+	BAUD_115200 =  26,
+};
+
+
+static struct Omap_uart_cfg {
+	Genode::addr_t mmio_base;
+	Genode::size_t mmio_size;
+	int            irq_number;
+} omap_uart_cfg[UARTS_NUM] = {
+	{ UART0_MMIO_BASE, UARTS_MMIO_SIZE, UART0_IRQ },
+	{ UART1_MMIO_BASE, UARTS_MMIO_SIZE, UART1_IRQ },
+	{ UART2_MMIO_BASE, UARTS_MMIO_SIZE, UART2_IRQ },
+	{ UART3_MMIO_BASE, UARTS_MMIO_SIZE, UART3_IRQ },
+};
+
+#endif /* _INCLUDE__PLATFORM__PANDABOARD__UART_DEFS_H_ */
---
+++ os/src/drivers/uart/omap4/main.cc
@@ -0,0 +1,83 @@
+/*
+ * \brief  Driver for OMAP4 UARTs
+ * \author Ivan Loskutov <ivan.loskutov@ksyslabs.org>
+ * \date   2012-09-28
+ */
+
+/*
+ * Copyright (C) 2012 Ksys Labs LLC
+ * Copyright (C) 2011-2012 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+/* Genode includes */
+#include <base/printf.h>
+#include <base/sleep.h>
+#include <os/config.h>
+#include <cap_session/connection.h>
+
+#include <uart_defs.h>
+
+/* local includes */
+#include "omap_uart.h"
+#include "terminal_component.h"
+
+
+int main(int argc, char **argv)
+{
+	using namespace Genode;
+
+	PDBG("--- OMAP4 UART driver started ---\n");
+
+	/**
+	 * Factory used by 'Terminal::Root' at session creation/destruction time
+	 */
+	struct Omap_uart_driver_factory : Terminal::Driver_factory
+	{
+		Omap_uart *created[UARTS_NUM];
+
+		/**
+		 * Constructor
+		 */
+		Omap_uart_driver_factory()
+		{
+			for (unsigned i = 0; i < UARTS_NUM; i++)
+				created[i] = 0;
+		}
+
+		Terminal::Driver *create(unsigned index,
+								 Terminal::Char_avail_callback &callback)
+		{
+			if (index > UARTS_NUM)
+				throw Terminal::Driver_factory::Not_available();
+
+			Omap_uart_cfg *cfg  = &omap_uart_cfg[index];
+			Omap_uart     *uart =  created[index];
+
+			if (!uart) {
+				uart = new (env()->heap())
+					Omap_uart(cfg->mmio_base, cfg->mmio_size, cfg->irq_number, BAUD_115200, callback);
+
+				/* update 'created' table */
+				created[index] = uart;
+			}
+
+			return uart;
+		}
+
+		void destroy(Terminal::Driver *driver) { /* TODO */ }
+
+	} driver_factory;
+
+	enum { STACK_SIZE = 0x2000 };
+	static Cap_connection cap;
+	static Rpc_entrypoint ep(&cap, STACK_SIZE, "uart_ep");
+
+	static Terminal::Root uart_root(&ep, env()->heap(), driver_factory);
+	env()->parent()->announce(ep.manage(&uart_root));
+
+	sleep_forever();
+	return 0;
+}
